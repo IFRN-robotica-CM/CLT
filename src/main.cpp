6f6527b5d57bbbf29eb159f558c135924b2b7a51
Robótica <samelazuck7@gmail.com>
2026-09-15 17:53:09 -0300

    atualização do código main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,43 @@
-#include <Arduino.h>
+#include <robo_hardware2.h> 
+#include <Servo.h>
+# include "Movimento.h"
 
-// put function declarations here:
-int myFunction(int, int);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+//-----PINOS PARA LIGAR MOTORES-----//
+//Motor1:
+//pino1 PWM_RODA_DIREITA  6
+//pino2 SENTIDO_RODA_DIREITA  7
+//
+//Motor2:
+//pino1 PWM_RODA_ESQUERDA   530
+//pino2 SENTIDO_RODA_ESQUERDA 4
+//-----PINOS PARA LIGAR MOTORES-----//
+
+//-----PINOS PARA SENSORES REFLETANCIA-----//
+//SENSOR_LINHA_MAIS_ESQUERDO      A3
+//SENSOR_LINHA_ESQUERDO         A2
+//SENSOR_LINHA_DIREITO          A1  
+//SENSOR_LINHA_MAIS_DIREITO     A0  
+//-----PINOS PARA SENSORES REFLETANCIA-----//
+
+
+
+#define DIVISOR_DE_DISTANCIA 15
+
+#include "SensorReflet.h"
+#include "Estrategia.h"
+
+SensorReflet sensorlinha;
+Movimento movimento;
+Estrategia estrategia;
+
+void setup(){
+  robo.configurar();
+  Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop(){
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  estrategia.executar();
+  
 }
